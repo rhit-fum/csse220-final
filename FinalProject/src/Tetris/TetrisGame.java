15a901012fdabaed10e982fb3488f7a36ad7611d
rhit-fum <fum@rose-hulman.edu>
2025-05-05 13:27:53 -0400

    successfully made the grids to appear

--- FinalProject/src/Tetris/TetrisGame.java
+++ FinalProject/src/Tetris/TetrisGame.java
@@ -10,6 +10,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class TetrisGame extends JPanel{
 	private int gameState; //gamestate: pause,continue, replay
+	private Cell[][] grid=new Cell[18][9]; //the grids in the window
+	private final int GRID_SIZE=48; //grid size is 48*48
 	//image of the blocks
 	public static BufferedImage I;
     public static BufferedImage J;
@@ -34,22 +36,44 @@ public class TetrisGame extends JPanel{
             System.out.print("image not found");
         }
     }
+    
     @Override
     protected void paintComponent(Graphics g) {
     	super.paintComponent(g);
     	Graphics2D g2=(Graphics2D) g;
     	g2.drawImage(background, 0, 0, this); //setup the backgroun image
-    	g2.drawImage(I, 0, 0, this); //test draw a block
+    	g2.translate(22, 15);
+    	drawGrid(g2);
+    	//g2.drawImage(I, 0, 0, this); //test draw a block, comment this line after
     }
+    //drawing the grids on the window
+    private void drawGrid(Graphics g) {
+    	for(int i=0;i<grid.length;i++) {
+    		for(int j=0;j<grid[i].length;j++) {
+    			int x=j*GRID_SIZE; //getting x and y position of each cell
+    			int y=i*GRID_SIZE;
+    			Cell cell=grid[i][j];
+    			if(cell==null) g.drawRect(x, y, GRID_SIZE, GRID_SIZE); //if empty, draw a square to represent the cell
+    			else g.drawImage(cell.getImage(), x, y, this); // if not empty, then there's should be a tetromino block
+    		}
+    	}
+    }
+    public void gameStart() {
+    	
+    }
+    
 	public static void main(String[] args) {
+		//setting up the JFrame and game panel
 		JFrame frame=new JFrame("Tetris Game");
-		TetrisGame panel=new TetrisGame();
-		frame.add(panel);
+		TetrisGame game=new TetrisGame();
+		frame.add(game);
 		frame.setVisible(true);
 		frame.setSize(810,940);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		
+		//start the game
+		game.gameStart();
 	}
 
 }
