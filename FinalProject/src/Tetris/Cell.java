4d52da1b89e07e7feefea6d403deab4ed30d98c8
rhit-fum <fum@rose-hulman.edu>
2025-05-24 16:22:41 -0400

    wrote the java doc for tetromino and cell class

--- FinalProject/src/Tetris/Cell.java
+++ FinalProject/src/Tetris/Cell.java
@@ -1,5 +1,10 @@
 package Tetris;
 import java.awt.image.BufferedImage;
+/**
+ * @author mingkun fu, Drake Bauernfeind
+ * purpose: This class is used for storing the information of the game board. A cell can either be empty(null), or filled with one of the blocks of a tetromino piece.
+ * when a tetromino piece  is landed on the ground, the corresponding cells will store the blocks of that piece.
+ */
 public class Cell {
 	private int row;
 	private int col; //the row & col of the block
@@ -10,33 +15,66 @@ public class Cell {
 		this.image=image;
 	}
 	//getter and setter methods
+	/**
+	 * ensures: get the current row of this particular cell
+	 * @return row
+	 */
 	public int getRow() {
 		return this.row;
 	}
+	/**
+	 * ensures: set the current row of this particular cell
+	 * @param row
+	 */
 	public void setRow(int row) {
 		this.row=row;
 	}
+	/**
+	 * ensures: get the current column of this particular cell
+	 * @return col
+	 */
 	public int getCol() {
 		return this.col;
 	}
+	/**
+	 * ensures: set the current column of this particular cell
+	 * @param col
+	 */
 	public void setCol(int col) {
 		this.col=col;
 	}
+	/**
+	 * ensure: get the texture for this cell
+	 * @return image
+	 */
 	public BufferedImage getImage() {
 		return image;
 	}
+	/**
+	 * ensures: set the texture for this cell
+	 * @param image
+	 */
 	public void setImage(BufferedImage image) {
 		this.image=image;
 	}
 	
 	
 	//movements
+	/**
+	 * ensures: move this cell leftwards
+	 */
 	public void left() {
 		col--;
 	}
+	/**
+	 * ensures: move this cell rightwards
+	 */
 	public void right() {
 		col++;
 	}
+	/**
+	 * ensures: move this cell downwards
+	 */
 	public void down() {
 		row++;
 	}
--- FinalProject/src/Tetris/Tetromino.java
+++ FinalProject/src/Tetris/Tetromino.java
@@ -1,36 +1,53 @@
 package Tetris;
 
 import java.awt.image.BufferedImage;
+/**
+ * class: Tetromino
+ * @author mingkun fu, Drake Bauernfeind
+ * purpose: the superclass for tetromino pieces which handles basic movement methods, it is inherited by subclasses which represents different shapes of tetromino pieces
+ */
 public class Tetromino {
 	//cell array has the information of the position of blocks
 	//each tetromino piece is consisted of 4 blocks, so an array of 4 elements
 	public Cell[] cells=new Cell[4]; 
 	
 	//movement methods
+	/**
+	 * ensures: move all the cells of the tetromino piece leftwards a unit
+	 */
 	public void moveLeft() {
 		for(Cell cell:cells) { //make all cells(AKA blocks) of the tetromino piece move left
 			cell.left();
 		}
 	}
-	
+	/**
+	 * ensures: move all the cells of the tetromino piece rightwards a unit
+	 */
 	public void moveRight() {
 		for(Cell cell:cells) {
 			cell.right();
 		}
 	}
-	
+	/**
+	 * ensures: move all the cells of the tetromino piece downwards a unit
+	 */
 	public void moveDown() {
 		for (Cell cell:cells) {
 			cell.down();
 		}
 	}
 	
-	//Method for placing a piece at the initial tile. Important for having pieces spawn at the top after being stored
+	/**
+	 * ensures:Method for placing a piece at the initial tile. Important for having pieces spawn at the top after being stored
+	 */
 	public void restartPiece() {
 		
 	}
 	
-	//generate tetromino pieces randomly
+	/**
+	 * ensures: generate tetromino pieces randomly
+	 * @return tetromino
+	 */
 	public static Tetromino randomPiece() {
         int num = (int) (Math.random() * 7);
         Tetromino tetromino = null;
@@ -60,7 +77,9 @@ public class Tetromino {
 
         return tetromino;
     }
-	
+	/**
+	 * ensure: rotate the tetromino piece clockwise
+	 */
 	public void rotate() {
 		for (Cell cell:cells) {
 			if (!cell.equals(cells[0])) {
@@ -73,7 +92,9 @@ public class Tetromino {
 			}
 		}
 	}
-	
+	/**
+	 * ensures: rotate the tetromino piece anti-clockwise to cancel the previous rotation
+	 */
 	public void cancelRotate() {
 		for (Cell cell:cells) {
 			if (!cell.equals(cells[0])) {
